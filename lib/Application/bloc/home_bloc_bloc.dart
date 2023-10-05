622fd701b53371263a782a9accef860623c4954f
Sreedev242 <sreedevkapparath@gmail.com>
2023-10-05 15:03:27 +0530

    2nd commit

--- lib/Application/bloc/home_bloc_bloc.dart
+++ lib/Application/bloc/home_bloc_bloc.dart
@@ -28,37 +28,7 @@ class HomeBlocBloc extends Bloc<HomeBlocEvent, HomeBlocState> {
       }
 
     });
-    on<_Fourg>((event, emit) async{
-       emit(HomeBlocState(resList: [], isLoading: true,buttonlist: []));
-      final Response response=await Dio(BaseOptions()).get('https://run.mocky.io/v3/f9b2dfb8-cf6e-4c5f-bc69-ec8bb2b5a03a');
-      try {
-        if (response.statusCode==200 || response.statusCode==201) {
-          final List XList=response.data['data'];
-       final cc=XList.where((element) => element['parent_group']=='4G').toList();
-      //  log(cc.toString());
-          final List<Map<String,dynamic>> Ylist=List<Map<String,dynamic>>.from(XList);
-          emit(HomeBlocState(resList: cc, isLoading: false,buttonlist: []));
-        }
-      } catch (e) {
-        print(e);
-      }
-    },);
-     on<_Fiveg>((event, emit) async{
-       emit(HomeBlocState(resList: [], isLoading: true,buttonlist: []));
-      final Response response=await Dio(BaseOptions()).get('https://run.mocky.io/v3/f9b2dfb8-cf6e-4c5f-bc69-ec8bb2b5a03a');
-      try {
-        if (response.statusCode==200 || response.statusCode==201) {
-          final List XList=response.data['data'];
-       final cc=XList.where((element) => element['parent_group']=='5G').toList();
-      //  log(cc.toString());
-          final List<Map<String,dynamic>> Ylist=List<Map<String,dynamic>>.from(XList);
-          emit(HomeBlocState(resList: cc, isLoading: false,buttonlist: []));
-        }
-      } catch (e) {
-        print(e);
-      }
-    },);
-
+   
     on<_ButtonBuild>((event, emit) async{
        emit(HomeBlocState(resList: [], isLoading: true,buttonlist: []));
       final Response response=await Dio(BaseOptions()).get('https://run.mocky.io/v3/f9b2dfb8-cf6e-4c5f-bc69-ec8bb2b5a03a');
@@ -73,8 +43,7 @@ class HomeBlocBloc extends Bloc<HomeBlocEvent, HomeBlocState> {
             }
        final List Glist=gSet.toList();
         
-        // final cc=data.where((element) => element['parent_group']==Glist[]).toList()
-      //  log(Glist.toString());
+       
        emit(HomeBlocState(resList:data , isLoading: false,buttonlist: Glist));
         }
       } catch (e) {
